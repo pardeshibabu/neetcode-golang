69547156a54c57846898445e4a89fcdc0e0e9298
pardeshibabu <pardeshikrm666@gail.com>
2023-07-04 23:10:32 +0530

    stack final commit

--- main.go
+++ main.go
@@ -15,18 +15,3 @@ func main() {
 	fmt.Println(stackneetcode.LargestRectangleArea(slice))
 	return
 }
-
-// permutation_in_string due
-// 0 [] -1
-// 1 [0] 0
-// 2 [1] 0
-// [1] 0
-// 3 [1 2] 1
-// [1 2] 1
-// 4 [1 2 3] 2
-// 5 [1 2 4] 2
-// [1 2 4] 2
-// [1 2 4 5]
-// stack
-// [1 0 0 4 0 0]
-// [1 0 0 4 0 0]
